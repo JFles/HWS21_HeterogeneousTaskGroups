108fce1c8d18826a3acadf311655a659b8c93ae7
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2021-08-03 11:13:22 -0500

    Initial Commit

---
+++ Day2_Session2_HeteroTaskGroups_Example/Day2_Session2_HeteroTaskGroups_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day2_Session2_HeteroTaskGroups_ExampleApp.swift
+//  Day2_Session2_HeteroTaskGroups_Example
+//
+//  Created by Jeremy Fleshman on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Day2_Session2_HeteroTaskGroups_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
